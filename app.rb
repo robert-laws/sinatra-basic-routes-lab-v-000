8ed293aff7f9893dbd424c3fb81d570c2b6aef38
Robert Laws <roblaws@me.com>
2017-10-23 16:18:21 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    'Hello, World!'
+  end
+
+  get '/name' do
+    'My name is Bob'
+  end
+
+  get '/hometown' do
+    'My hometown is USA'
+  end
+
+  get '/favorite-song' do
+    'My favorite song is fun song'
+  end
 end
